f18e3d1a7409ea467207a20b32cbf6c6406f07d9
hzahid-at-589736534170 <you@example.com>
2021-07-03 20:56:52 +0500

    all6

--- .platform/hooks/postdeploy/01_update_ng_conf.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# Replace the original nginx.conf by our custom one
-sudo mv /tmp/custom_nginx.conf /etc/nginx/conf.d/
-
-# Restart nginx to apply modifications
-sudo service nginx restart
